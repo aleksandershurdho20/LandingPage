f872ecfc7d099ccfada2e0f611d9dfb6e80f64ee
aleksandershurdho20 <aleksandershurdho@gmail.com>
2020-06-03 11:45:47 +0200

    refactored into php

--- index.php
+++ index.php
@@ -1,3 +1,102 @@
+<?php
+//index.php
+
+$error = '';
+$name = '';
+$email = '';
+$subject = '';
+$message = '';
+
+function clean_text($string)
+{
+	$string = trim($string);
+	$string = stripslashes($string);
+	$string = htmlspecialchars($string);
+	return $string;
+}
+
+if(isset($_POST["submit"]))
+{
+	if(empty($_POST["name"]))
+	{
+		$error .= '<p><label class="text-danger">Please Enter your Name</label></p>';
+	}
+	else
+	{
+		$name = clean_text($_POST["name"]);
+		if(!preg_match("/^[a-zA-Z ]*$/",$name))
+		{
+			$error .= '<p><label class="text-danger">Only letters and white space allowed</label></p>';
+		}
+	}
+	if(empty($_POST["email"]))
+	{
+		$error .= '<p><label class="text-danger">Please Enter your Email</label></p>';
+	}
+	else
+	{
+		$email = clean_text($_POST["email"]);
+		if(!filter_var($email, FILTER_VALIDATE_EMAIL))
+		{
+			$error .= '<p><label class="text-danger">Invalid email format</label></p>';
+		}
+	}
+	if(empty($_POST["subject"]))
+	{
+		$error .= '<p><label class="text-danger">Subject is required</label></p>';
+	}
+	else
+	{
+		$subject = clean_text($_POST["subject"]);
+	}
+	if(empty($_POST["message"]))
+	{
+		$error .= '<p><label class="text-danger">Message is required</label></p>';
+	}
+	else
+	{
+		$message = clean_text($_POST["message"]);
+	}
+	if($error == '')
+	{
+		require 'class/class.phpmailer.php';
+		$mail = new PHPMailer;
+		$mail->IsSMTP();								//Sets Mailer to send message using SMTP
+		$mail->Host = 'smtp.mailtrap.io';		//Sets the SMTP hosts of your Email hosting, this for Godaddy
+		$mail->Port = '2525';								//Sets the default SMTP server port
+		$mail->SMTPAuth = true;							//Sets SMTP authentication. Utilizes the Username and Password variables
+		$mail->Username = '20bf21979e9cc9';					//Sets SMTP username
+		$mail->Password = '18a47d18218419';					//Sets SMTP password
+		$mail->SMTPSecure = '';							//Sets connection prefix. Options are "", "ssl" or "tls"
+		$mail->From = $_POST["email"];					//Sets the From email address for the message
+		$mail->FromName = $_POST["name"];				//Sets the From name of the message
+		$mail->AddAddress($_POST["email"], $_POST["name"]);		//Adds a "To" address
+		$mail->AddCC($_POST["email"], $_POST["name"]);	//Adds a "Cc" address
+		$mail->WordWrap = 50;							//Sets word wrapping on the body of the message to a given number of characters
+		$mail->IsHTML(true);							//Sets message type to HTML				
+		$mail->Subject = $_POST["subject"];				//Sets the Subject of the message
+		$mail->Body = $_POST["message"];				//An HTML or plain text message body
+		if($mail->Send())								//Send an Email. Return true on success or false on error
+		{
+			$error = '<label class="text-success">Thank you for contacting us</label>';
+		}
+		else
+		{
+			$error = '<label class="text-danger">There is an Error</label>';
+		}
+		$name = '';
+		$email = '';
+		$subject = '';
+		$message = '';
+	}
+}
+
+?>
+
+
+
+
+
 <!DOCTYPE html>
 <head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
@@ -188,7 +287,7 @@
         </div>
     </div>
     <div class="container contact-form" id="contact">
-        <form method="POST">
+        <form action ="index.php" method="post">
             <h3 class="text-center">COMPILA IL MODULO</h3>
             <div class="form-group">
                 <label>Nome e Cognome</label>
@@ -238,12 +337,12 @@
              </div>
              <div class="form-group">
                 <label>Note per il corriere</label>
-                <input type="text" name="notes" class="form-control" rows="2" placeholder="note">
+                <input type="text" name="message" class="form-control" rows="2" placeholder="note">
              </div>
              <div class="d-flex justify-content-center">
                 <div id="info-privacy"><small>Cliccando "Completa l'acquisto" confermi di aver preso visione <a href="#">dell'informativa sulla privacy</a>.</small></div>
              </div>
-             <button id="submit-button" class="btn btn-lg btn-warning mt-3" type="submit">Completa l'acquisto</button>
+             <button id="submit-button" class="btn btn-lg btn-warning mt-3" name="submit" type="submit">Completa l'acquisto</button>
              <div class="contact-seperator"></div>
         </form>
     </div>
@@ -262,7 +361,7 @@
       var sec = Math.floor((countdown - (min * 60 * 1000)) / 1000); 
     
       if (countdown <= 0) {
-         alert("30 min!");
+        //  alert("offer finished!");
          clearInterval(timerId);
       } else {
          $("#counter").html(min + " : " + sec);
